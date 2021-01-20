ca30076602273180ae14bba6883ffe4dd99ea9c4
KIET <kensugisaki94@gmail.com>
2021-01-20 20:47:30 +0700

    day 1

--- WindowsFormsSample/Form1.Designer.cs
+++ WindowsFormsSample/Form1.Designer.cs
@@ -29,24 +29,270 @@ namespace WindowsFormsApp2
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode28 = new System.Windows.Forms.TreeNode("Node2");
+            System.Windows.Forms.TreeNode treeNode29 = new System.Windows.Forms.TreeNode("Node7");
+            System.Windows.Forms.TreeNode treeNode30 = new System.Windows.Forms.TreeNode("Node8");
+            System.Windows.Forms.TreeNode treeNode31 = new System.Windows.Forms.TreeNode("Node1", new System.Windows.Forms.TreeNode[] {
+            treeNode28,
+            treeNode29,
+            treeNode30});
+            System.Windows.Forms.TreeNode treeNode32 = new System.Windows.Forms.TreeNode("Node3");
+            System.Windows.Forms.TreeNode treeNode33 = new System.Windows.Forms.TreeNode("Node4");
+            System.Windows.Forms.TreeNode treeNode34 = new System.Windows.Forms.TreeNode("Node5");
+            System.Windows.Forms.TreeNode treeNode35 = new System.Windows.Forms.TreeNode("Node6");
+            System.Windows.Forms.TreeNode treeNode36 = new System.Windows.Forms.TreeNode("Node0", new System.Windows.Forms.TreeNode[] {
+            treeNode31,
+            treeNode32,
+            treeNode33,
+            treeNode34,
+            treeNode35});
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txtNumberA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbCheckBox = new System.Windows.Forms.CheckBox();
+            this.cbbListNumber = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txtNumberA
+            // 
+            this.txtNumberA.Location = new System.Drawing.Point(259, 45);
+            this.txtNumberA.Name = "txtNumberA";
+            this.txtNumberA.Size = new System.Drawing.Size(179, 20);
+            this.txtNumberA.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(223, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "So A";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(249, 150);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(114, 52);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(135, 97);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(336, 20);
+            this.txtResult.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(92, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Result";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "So luong";
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(145, 45);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(56, 20);
+            this.txtCount.TabIndex = 9;
+            this.txtCount.TextChanged += new System.EventHandler(this.txtCount_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(451, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 20);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbCheckBox
+            // 
+            this.cbCheckBox.AutoSize = true;
+            this.cbCheckBox.Checked = true;
+            this.cbCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbCheckBox.Location = new System.Drawing.Point(517, 100);
+            this.cbCheckBox.Name = "cbCheckBox";
+            this.cbCheckBox.Size = new System.Drawing.Size(47, 17);
+            this.cbCheckBox.TabIndex = 12;
+            this.cbCheckBox.Text = "Test";
+            this.cbCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // cbbListNumber
+            // 
+            this.cbbListNumber.FormattingEnabled = true;
+            this.cbbListNumber.Location = new System.Drawing.Point(71, 150);
+            this.cbbListNumber.Name = "cbbListNumber";
+            this.cbbListNumber.Size = new System.Drawing.Size(121, 21);
+            this.cbbListNumber.TabIndex = 14;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(429, 234);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(135, 310);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(384, 151);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 16;
+            this.dateTimePicker1.Value = new System.DateTime(2021, 1, 1, 0, 0, 0, 0);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(41, 234);
+            this.treeView1.Name = "treeView1";
+            treeNode28.Name = "Node2";
+            treeNode28.Text = "Node2";
+            treeNode29.Name = "Node7";
+            treeNode29.Text = "Node7";
+            treeNode30.Name = "Node8";
+            treeNode30.Text = "Node8";
+            treeNode31.Name = "Node1";
+            treeNode31.Text = "Node1";
+            treeNode32.Name = "Node3";
+            treeNode32.Text = "Node3";
+            treeNode33.Name = "Node4";
+            treeNode33.Text = "Node4";
+            treeNode34.Name = "Node5";
+            treeNode34.Text = "Node5";
+            treeNode35.Name = "Node6";
+            treeNode35.Text = "Node6";
+            treeNode36.Name = "Node0";
+            treeNode36.Text = "Node0";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode36});
+            this.treeView1.Size = new System.Drawing.Size(241, 310);
+            this.treeView1.TabIndex = 17;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp2.Properties.Resources.Andersen;
+            this.pictureBox1.Location = new System.Drawing.Point(643, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(318, 532);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(987, 589);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cbbListNumber);
+            this.Controls.Add(this.cbCheckBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumberA);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox txtNumberA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox cbCheckBox;
+        private System.Windows.Forms.ComboBox cbbListNumber;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- WindowsFormsSample/Form1.cs
+++ WindowsFormsSample/Form1.cs
@@ -12,6 +12,9 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        List<int> listNumber = new List<int>();
+        int soLan = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +22,70 @@ namespace WindowsFormsApp2
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            dateTimePicker1.Value = DateTime.Now;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                DialogResult rs = dialog.ShowDialog();
+                if(rs == DialogResult.OK)
+                {
+                    string path = dialog.FileName;
+                    Image img = Bitmap.FromFile(path);
+                    pictureBox1.Image = img;
+                    Graphics g = Graphics.FromImage(img);
+                }
+
+                bool check = cbCheckBox.Checked;
+                if(check == true)
+                {
+                    int result = 0;
+                    foreach (var item in listNumber)
+                    {
+                        result = result + item;
+                    }
+                    txtResult.Text = result.ToString();
+                }
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Nhap sai");
+            }
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int soLan = Convert.ToInt32(txtCount.Text);
+            if (listNumber.Count < Convert.ToInt32(txtCount.Text))
+            {
+                Calculate();
+            }
+
+
+            
+
+        }
+
+        private void txtCount_TextChanged(object sender, EventArgs e)
+        {
+            soLan = Convert.ToInt32(txtCount.Text);
+        }
+
+        public void Calculate()
+        {
+            Rectangle rec = new Rectangle();
+            int number = Convert.ToInt32(txtNumberA.Text);
+            listNumber.Add(number);
+            cbbListNumber.Items.Add(number);
+            MessageBox.Show("Da nhap " + number);
+        }
     }
+
+   
 }
