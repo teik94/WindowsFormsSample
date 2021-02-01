7a2dd838aca10ea4f56a724ade5784da1153f3f3
KIET <kensugisaki94@gmail.com>
2021-02-01 21:16:55 +0700

    product management

---
+++ WindowsFormsSample/Form3.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nmrPrice = new System.Windows.Forms.NumericUpDown();
+            this.nmrQuantity = new System.Windows.Forms.NumericUpDown();
+            this.cbPromotion = new System.Windows.Forms.CheckBox();
+            this.cbbPType = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmrPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmrQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 186);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(753, 272);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Product Code";
+            // 
+            // txtPCode
+            // 
+            this.txtPCode.Location = new System.Drawing.Point(95, 24);
+            this.txtPCode.Name = "txtPCode";
+            this.txtPCode.Size = new System.Drawing.Size(395, 20);
+            this.txtPCode.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Product Name";
+            // 
+            // txtPName
+            // 
+            this.txtPName.Location = new System.Drawing.Point(95, 50);
+            this.txtPName.Name = "txtPName";
+            this.txtPName.Size = new System.Drawing.Size(395, 20);
+            this.txtPName.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Quantity";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Note";
+            // 
+            // txtNote
+            // 
+            this.txtNote.Location = new System.Drawing.Point(95, 128);
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(395, 20);
+            this.txtNote.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.cbbPType);
+            this.groupBox1.Controls.Add(this.cbPromotion);
+            this.groupBox1.Controls.Add(this.nmrQuantity);
+            this.groupBox1.Controls.Add(this.nmrPrice);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtNote);
+            this.groupBox1.Controls.Add(this.txtPCode);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtPName);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(533, 168);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Product";
+            // 
+            // nmrPrice
+            // 
+            this.nmrPrice.Increment = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nmrPrice.Location = new System.Drawing.Point(95, 77);
+            this.nmrPrice.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.nmrPrice.Name = "nmrPrice";
+            this.nmrPrice.Size = new System.Drawing.Size(120, 20);
+            this.nmrPrice.TabIndex = 3;
+            // 
+            // nmrQuantity
+            // 
+            this.nmrQuantity.Location = new System.Drawing.Point(95, 103);
+            this.nmrQuantity.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nmrQuantity.Name = "nmrQuantity";
+            this.nmrQuantity.Size = new System.Drawing.Size(120, 20);
+            this.nmrQuantity.TabIndex = 3;
+            // 
+            // cbPromotion
+            // 
+            this.cbPromotion.AutoSize = true;
+            this.cbPromotion.Location = new System.Drawing.Point(245, 78);
+            this.cbPromotion.Name = "cbPromotion";
+            this.cbPromotion.Size = new System.Drawing.Size(73, 17);
+            this.cbPromotion.TabIndex = 5;
+            this.cbPromotion.Text = "Promotion";
+            this.cbPromotion.UseVisualStyleBackColor = true;
+            // 
+            // cbbPType
+            // 
+            this.cbbPType.FormattingEnabled = true;
+            this.cbbPType.Items.AddRange(new object[] {
+            "Quần",
+            "Áo",
+            "Vớ",
+            "Giày"});
+            this.cbbPType.Location = new System.Drawing.Point(282, 102);
+            this.cbbPType.Name = "cbbPType";
+            this.cbbPType.Size = new System.Drawing.Size(121, 21);
+            this.cbbPType.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(245, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Type";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(561, 157);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(642, 157);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 475);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmrPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmrQuantity)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox cbPromotion;
+        private System.Windows.Forms.NumericUpDown nmrQuantity;
+        private System.Windows.Forms.NumericUpDown nmrPrice;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbbPType;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ WindowsFormsSample/Form3.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string code = txtPCode.Text;
+            string name = txtPName.Text;
+            int price = Convert.ToInt32(nmrPrice.Value);
+            int quantity = Convert.ToInt32(nmrQuantity.Value);
+            string note = txtNote.Text;
+            bool promotion = cbPromotion.Checked;
+            string type = cbbPType.Text;
+
+            if(code == "")
+            {
+                MessageBox.Show("Code cannot be empty");
+                return;
+            }
+            else if (name == "")
+            {
+                MessageBox.Show("Product Name cannot be empty");
+                return;
+            }
+
+            DataTable dt = (DataTable)dataGridView1.DataSource;
+            if(dt == null)
+            {
+                DataTable newDt = new DataTable();
+                newDt.Columns.Add("Code");
+                newDt.Columns.Add("Name");
+                newDt.Columns.Add("Price");
+                newDt.Columns.Add("Quantity");
+                newDt.Columns.Add("Promotion");
+                newDt.Columns.Add("Type");
+                newDt.Columns.Add("Note");
+
+                DataRow dr = newDt.NewRow();
+                dr["Code"] = code;
+                dr["Name"] = name;
+                dr["Price"] = price;
+                dr["Quantity"] = quantity;
+                dr["Promotion"] = promotion;
+                dr["Type"] = type;
+                dr["Note"] = note;
+                newDt.Rows.Add(dr);
+
+                dt = newDt;
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                bool found = false;
+                foreach (DataRow row in dt.Rows)
+                {
+                    if(row["Code"].ToString() == code)
+                    {
+                        row["Name"] = name;
+                        row["Price"] = price;
+                        row["Quantity"] = quantity;
+                        row["Promotion"] = promotion;
+                        row["Type"] = type;
+                        row["Note"] = note;
+                        found = true;
+                        break;
+                    }
+                }
+                if(found == false)
+                {
+                    DataRow dr = dt.NewRow();
+                    dr["Code"] = code;
+                    dr["Name"] = name;
+                    dr["Price"] = price;
+                    dr["Quantity"] = quantity;
+                    dr["Promotion"] = promotion;
+                    dr["Type"] = type;
+                    dr["Note"] = note;
+                    dt.Rows.Add(dr);
+                }
+
+                //dataGridView1.DataSource = dt;
+            }
+
+            WriteFile(@"C:\KIET\data.kiet", dt);
+
+        }
+
+        public void WriteFile(string path, DataTable data)
+        {
+            FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            //TypeConverter obj = TypeDescriptor.GetConverter(data.GetType());
+            //byte[] bt = (byte[])obj.ConvertTo(data, typeof(byte[]));
+
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bf.Serialize(ms, data);
+                byte[] bt = ms.ToArray();
+                fs.Write(bt, 0, bt.Length);
+            }
+
+            fs.Close();
+        }
+
+        public DataTable ReadFile(string path)
+        {
+            //FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+            BinaryFormatter bf = new BinaryFormatter();
+            using (var ms = new MemoryStream(bytes))
+            {
+                DataTable obj = (DataTable)bf.Deserialize(ms);
+                //fs.Close();
+                return obj;
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataGridViewRow dr = dataGridView1.SelectedRows[0];
+                dataGridView1.Rows.Remove(dr);
+                DataTable dt = (DataTable)dataGridView1.DataSource;
+                WriteFile(@"C:\KIET\data.kiet", dt);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            //DataTable dt = (DataTable)dataGridView1.DataSource;
+            try
+            {
+                DataGridViewRow dr = dataGridView1.SelectedRows[0];
+                txtPCode.Text = dr.Cells["Code"].Value.ToString();
+                txtPName.Text = dr.Cells["Name"].Value.ToString();
+                nmrPrice.Value = Convert.ToDecimal(dr.Cells["Price"].Value);
+                nmrQuantity.Value = Convert.ToDecimal(dr.Cells["Quantity"].Value);
+                cbPromotion.Checked = Convert.ToBoolean(dr.Cells["Promotion"].Value);
+                cbbPType.Text = dr.Cells["Type"].Value.ToString();
+                txtNote.Text = dr.Cells["Note"].Value.ToString();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if(File.Exists(@"C:\KIET\data.kiet"))
+                {
+                    DataTable dt = ReadFile(@"C:\KIET\data.kiet");
+                    dataGridView1.DataSource = dt;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
+        }
+    }
+}
