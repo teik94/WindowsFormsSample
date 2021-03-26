b69943fd5ead291ef254e711e88e8f803dce24f2
KIET <kensugisaki94@gmail.com>
2021-03-26 21:02:51 +0700

    Back - Next - Up Address Bar

--- WindowsFormsSample/Form4.Designer.cs
+++ WindowsFormsSample/Form4.Designer.cs
@@ -30,10 +30,7 @@ namespace WindowsFormsApp2
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
-            this.btnDeleteRow = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.button2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.listView1 = new System.Windows.Forms.ListView();
@@ -46,49 +43,27 @@ namespace WindowsFormsApp2
             this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.largeIconToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.smallIconToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 40);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Add Data";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // btnDeleteRow
-            // 
-            this.btnDeleteRow.BackColor = System.Drawing.Color.Yellow;
-            this.btnDeleteRow.Location = new System.Drawing.Point(112, 12);
-            this.btnDeleteRow.Name = "btnDeleteRow";
-            this.btnDeleteRow.Size = new System.Drawing.Size(100, 39);
-            this.btnDeleteRow.TabIndex = 2;
-            this.btnDeleteRow.Text = "Delete Row";
-            this.btnDeleteRow.UseVisualStyleBackColor = false;
-            this.btnDeleteRow.Click += new System.EventHandler(this.btnDeleteRow_Click);
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 622);
+            this.progressBar1.Location = new System.Drawing.Point(12, 645);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(1269, 23);
             this.progressBar1.Step = 1;
             this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
             this.progressBar1.TabIndex = 3;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(259, 13);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 39);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // timer1
             // 
@@ -97,10 +72,12 @@ namespace WindowsFormsApp2
             // 
             // treeView1
             // 
+            this.treeView1.HideSelection = false;
             this.treeView1.Location = new System.Drawing.Point(12, 69);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(371, 535);
+            this.treeView1.Size = new System.Drawing.Size(266, 570);
             this.treeView1.TabIndex = 5;
+            this.treeView1.BeforeSelect += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeSelect);
             this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
             // listView1
@@ -116,38 +93,124 @@ namespace WindowsFormsApp2
             this.columnHeader8,
             this.columnHeader9});
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(389, 69);
+            this.listView1.Location = new System.Drawing.Point(284, 69);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(879, 535);
+            this.listView1.Size = new System.Drawing.Size(984, 570);
             this.listView1.TabIndex = 6;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1293, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.largeIconToolStripMenuItem,
+            this.smallIconToolStripMenuItem,
+            this.listToolStripMenuItem,
+            this.detailToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // largeIconToolStripMenuItem
+            // 
+            this.largeIconToolStripMenuItem.Name = "largeIconToolStripMenuItem";
+            this.largeIconToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.largeIconToolStripMenuItem.Text = "Large Icon";
+            // 
+            // smallIconToolStripMenuItem
+            // 
+            this.smallIconToolStripMenuItem.Name = "smallIconToolStripMenuItem";
+            this.smallIconToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.smallIconToolStripMenuItem.Text = "Small Icon";
+            // 
+            // listToolStripMenuItem
+            // 
+            this.listToolStripMenuItem.Name = "listToolStripMenuItem";
+            this.listToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.listToolStripMenuItem.Text = "List";
+            // 
+            // detailToolStripMenuItem
+            // 
+            this.detailToolStripMenuItem.Name = "detailToolStripMenuItem";
+            this.detailToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.detailToolStripMenuItem.Text = "Detail";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(145, 43);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(1123, 20);
+            this.txtPath.TabIndex = 8;
+            this.txtPath.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPath_KeyDown);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(50, 40);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(32, 23);
+            this.btnNext.TabIndex = 9;
+            this.btnNext.Text = "🡺";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(12, 40);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(32, 23);
+            this.btnBack.TabIndex = 9;
+            this.btnBack.Text = "🡸";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(107, 41);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(32, 23);
+            this.btnUp.TabIndex = 9;
+            this.btnUp.Text = "🡹";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1293, 680);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.txtPath);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.treeView1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.btnDeleteRow);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "Form4";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form4";
             this.Load += new System.EventHandler(this.Form4_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btnDeleteRow;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.ListView listView1;
@@ -160,5 +223,15 @@ namespace WindowsFormsApp2
         private System.Windows.Forms.ColumnHeader columnHeader7;
         private System.Windows.Forms.ColumnHeader columnHeader8;
         private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem largeIconToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem smallIconToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detailToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnUp;
     }
 }
--- WindowsFormsSample/Form4.cs
+++ WindowsFormsSample/Form4.cs
@@ -19,7 +19,10 @@ namespace WindowsFormsApp2
         //List<Card> listCard;
         BindingList<Card> listCard = new BindingList<Card>();
         const string savePath = @"C:\KIET\data2.kiet";
-
+        //TreeNode lastNode;
+        TreeNode selectedNode;
+        List<TreeNode> lastNode = new List<TreeNode>();
+        List<TreeNode> nextNode = new List<TreeNode>();
 
         public Form4()
         {
@@ -36,10 +39,12 @@ namespace WindowsFormsApp2
             {
                 if (drive.Name != "C:\\")
                 {
-                    string folderPath = drive.Name;
+                    string folderPath = drive.Name.Replace("\\", "");
 
                     TreeNode root = new TreeNode();
                     root.Text = folderPath;
+                    //root.Name = folderPath;
+                    root.Name = drive.Name;
                     GetFolder(folderPath, root);
                     treeView1.Nodes.Add(root);
                 }
@@ -47,22 +52,75 @@ namespace WindowsFormsApp2
 
 
             ImageList imgList = new ImageList();
-            imgList.Images.Add(Image.FromFile(@"E:\Games\folder-icon.png"));
-            imgList.Images.Add(Image.FromFile(@"E:\Games\file-icon.png"));
-            imgList.Images.Add(Image.FromFile(@"E:\Games\image-file-icon.png"));
+            imgList.Images.Add(Image.FromFile(@".\Resources\Icon\folder-icon.png"));
+            imgList.Images.Add(Image.FromFile(@".\Resources\Icon\file-icon.png"));
+            imgList.Images.Add(Image.FromFile(@".\Resources\Icon\image-file-icon.png"));
             imgList.ImageSize = new Size(64, 64);
             listView1.LargeImageList = imgList;
 
+            treeView1.SelectedNode = treeView1.Nodes[0];
+            lastNode.Add(treeView1.SelectedNode);
+        }
 
+        private void treeView1_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            if(selectedNode != null && lastNode.Count > 0 && selectedNode != lastNode.Last())
+            {
+                lastNode.Add(selectedNode);
+                nextNode.Clear();
+            }
+           
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if(lastNode.Count > 0)
+            {
+                nextNode.Add(selectedNode);
+                selectedNode = null;
+
+                treeView1.Focus();
+                treeView1.SelectedNode = lastNode.Last();
+
+                if(lastNode.Count > 1)
+                {
+                    lastNode.Remove(lastNode.Last());
+                }
+                
+            }
+            
+
+        }
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            
+            treeView1.Focus();
+            treeView1.SelectedNode = treeView1.SelectedNode.Parent;
 
         }
 
-        TreeNode selectedNode;
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if(nextNode.Count > 0)
+            {
+                lastNode.Add(selectedNode);
+                selectedNode = null;
+
+                treeView1.Focus();
+                treeView1.SelectedNode = nextNode.Last();
+
+                nextNode.Remove(nextNode.Last());
+            }
+            
+        }
+
 
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
             selectedNode = e.Node;
             string path = e.Node.FullPath.Replace(@"\\", @"\");
+            txtPath.Text = path;
             List<string> files = new List<string>(Directory.GetFiles(path));
             List<string> subFolders = Directory.EnumerateDirectories(path).ToList();
             listView1.Items.Clear();
@@ -110,6 +168,7 @@ namespace WindowsFormsApp2
                     {
                         if (node.Text == choosen.Text)
                         {
+                            //treeView1.Focus();
                             treeView1.SelectedNode = node;
                             break;
                         }
@@ -138,7 +197,14 @@ namespace WindowsFormsApp2
                 foreach (string item in subFolders)
                 {
                     DirectoryInfo di = new DirectoryInfo(item);
-                    TreeNode child = node.Nodes.Add(di.Name);
+
+                    TreeNode child = new TreeNode();
+                    child.Text = di.Name;
+                    //child.Name = di.Name;
+                    child.Name = di.FullName;
+
+                    node.Nodes.Add(child);
+
                     //foreach (var file in files)
                     //{
                     //   //FileInfo fi = new FileInfo(file);
@@ -203,6 +269,67 @@ namespace WindowsFormsApp2
             }
         }
 
-        
+        private void txtPath_TextChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Changed");
+        }
+
+        private void txtPath_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                TextBox txt = (TextBox)sender;
+                string path = txt.Text;
+
+                if(path.Last() == '\\')
+                {
+                    path = path.Remove(path.Length - 1, 1);
+                }
+
+                //char c = '\\';
+                //string[] test = path.Split(c);
+                //List<string> searchNames = new List<string>();
+                //foreach (var item in test)
+                //{
+                //    if(item != "")
+                //    {
+                //        searchNames.Add(item);
+                //    }
+                //}
+
+                //TreeNode node = FindNode(treeView1.Nodes, searchNames);
+                TreeNode[] node = treeView1.Nodes.Find(path, true);
+
+                if (node.Length == 0)
+                {
+                    MessageBox.Show("Path not found!");
+                }
+                else
+                {
+                    treeView1.Focus();
+                    treeView1.SelectedNode = node[0];
+                }
+                
+            }
+        }
+
+        private TreeNode FindNode(TreeNodeCollection nodes, List<string> names)
+        {
+            var nodeFound = nodes.Find(names[0], false);
+            if(nodeFound.Length > 0)
+            {
+                names.RemoveAt(0);
+                if (names.Count > 0 && nodeFound[0].Nodes.Count > 0)
+                {
+                    return FindNode(nodeFound[0].Nodes, names);
+                }
+                else if (names.Count == 0)
+                {
+                    return nodeFound[0];
+                }
+            }
+
+            return null;
+        }
     }
 }
